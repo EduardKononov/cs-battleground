aebf4a1411e7f46522bc2c0e27b162bcb61abc66
Eduard Kononov <aduard.kononov@gmail.com>
2021-02-10 18:06:53 +0300

    first robot test

--- main_test.py
+++ main_test.py
@@ -1,32 +1,14 @@
 import b0RemoteApi
 import time
 
-with b0RemoteApi.RemoteApiClient('b0RemoteApi_pythonClient', 'b0RemoteApi') as client:
-    doNextStep = True
-
-
-    def simulationStepStarted(msg):
-        simTime = msg[1][b'simulationTime']
-        print('Simulation step started. Simulation time: ', simTime)
-
-
-    def simulationStepDone(msg):
-        simTime = msg[1][b'simulationTime']
-        print('Simulation step done. Simulation time: ', simTime)
-        global doNextStep
-        doNextStep = True
-
-
-    client.simxSynchronous(True)
-    client.simxGetSimulationStepStarted(client.simxDefaultSubscriber(simulationStepStarted))
-    client.simxGetSimulationStepDone(client.simxDefaultSubscriber(simulationStepDone))
-    client.simxStartSimulation(client.simxDefaultPublisher())
-
-    startTime = time.time()
-    while time.time() < startTime + 5:
-        if doNextStep:
-            doNextStep = False
-            client.simxSynchronousTrigger()
-        client.simxSpinOnce()
-
-    client.simxStopSimulation(client.simxDefaultPublisher())
+with b0RemoteApi.RemoteApiClient() as client:
+    sc = client.simxServiceCall()
+    try:
+        client.simxStartSimulation(sc)
+        _, leftMotor = client.simxGetObjectHandle('Pioneer_p3dx_leftMotor', sc)
+        _, rightMotor = client.simxGetObjectHandle('Pioneer_p3dx_rightMotor', sc)
+        client.simxSetJointTargetVelocity(leftMotor, 1, sc)
+        client.simxSetJointTargetVelocity(rightMotor, 1, sc)
+        time.sleep(100)
+    finally:
+        client.simxStopSimulation(sc)
