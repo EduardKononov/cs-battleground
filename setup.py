14881a1740b1bb04bb2de33d03419f3e7c9f7e45
Eduard Kononov <aduard.kononov@gmail.com>
2021-02-11 14:04:57 +0300

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='cs-battleground',
+    version='0.0.1',
+    author='Eduard Konanau & Dzmitry Zavaliony',
+    author_email='aduard.kononov@gmail.com',
+    description='CoppeliaSim based robot battleground',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/ekon-university/8_sem_labs/pis/battleground',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=required,
+)
