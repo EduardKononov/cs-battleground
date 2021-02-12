2b3399bdeca573dd75665f888da9e8acc0bc94a4
Eduard Kononov <aduard.kononov@gmail.com>
2021-02-12 22:37:42 +0300

    adds --no-comments flag

--- cs_battleground/init/__main__.py
+++ cs_battleground/init/__main__.py
@@ -1,9 +1,14 @@
 import os
+import re
+import sys
 import shutil
+from tempfile import NamedTemporaryFile
 from pathlib import Path
 
 
 def main():
+    no_comments = '--no-comments' in sys.argv
+
     cwd = Path(os.getcwd())
 
     cur_dir = Path(__file__).absolute().parent
@@ -12,6 +17,18 @@ def main():
         for path in cur_dir.glob('*')
         if '__' not in str(path)
     ]:
+        if path.suffix == '.py' and no_comments:
+            with path.open('r') as file:
+                text = file.read()
+            cleaned = re.sub(r'(.*#.*\n|\n.*""".*""")', '', text).strip() + '\n'
+
+            with NamedTemporaryFile('w+') as file:
+                file.write(cleaned)
+                file.seek(0)
+                temp_path = Path(file.name).absolute()
+                shutil.copy(temp_path, cwd / path.name)
+            continue
+
         shutil.copy(path, cwd / path.name)
 
 
