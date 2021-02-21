07649ffca2e501a06a355c7c4c972add87288386
Эдуард Кононов <aduard.kononov@gmail.com>
2021-02-21 07:41:09 +0000

    fixes temp ifle issue

--- cs_battleground/cli/__main__.py
+++ cs_battleground/cli/__main__.py
@@ -30,7 +30,32 @@ def clean(path, cleaners):
     for cleaner in cleaners:
         cleaned = cleaner(cleaned)
 
-    with NamedTemporaryFile('w+') as file:
+    class CustomNamedTemporaryFile:
+        """
+        This custom implementation is needed because of the following limitation of tempfile.NamedTemporaryFile:
+
+        > Whether the name can be used to open the file a second time, while the named temporary file is still open,
+        > varies across platforms (it can be so used on Unix; it cannot on Windows NT or later).
+        """
+        def __init__(self, mode='wb', delete=True):
+            self._mode = mode
+            self._delete = delete
+
+        def __enter__(self):
+            # Generate a random temporary file name
+            file_name = os.path.join(tempfile.gettempdir(), os.urandom(24).hex())
+            # Ensure the file is created
+            open(file_name, "x").close()
+            # Open the file in the given mode
+            self._tempFile = open(file_name, self._mode)
+            return self._tempFile
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self._tempFile.close()
+            if self._delete:
+                os.remove(self._tempFile.name)
+
+    with CustomNamedTemporaryFile('w+') as file:
         file.write(cleaned)
         file.seek(0)
         temp_path = Path(file.name).absolute()
